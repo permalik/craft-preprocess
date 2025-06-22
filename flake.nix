c8f10a96498e6652287de78f32f817698f842a91
permalik <permalik@protonmail.com>
2025-06-21 19:36:54 -0500

    feat: init

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "blue-preprocess";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pname = "blue-preprocess";
+        version = "0.0.1";
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = false;
+        };
+      in {
+        packages.default = pkgs.buildGoModule {
+          inherit pname version;
+          src = ./.;
+          vendorHash = null;
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.alejandra
+            pkgs.go_1_23
+            pkgs.golangci-lint
+            pkgs.pre-commit
+          ];
+
+          shellHook = ''
+            # Source .bashrc
+            . .bashrc
+
+            # Tooling
+            go install golang.org/x/tools/cmd/goimports@latest
+          '';
+        };
+      }
+    );
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/permalik/blue-preprocess
+
+go 1.23.6
